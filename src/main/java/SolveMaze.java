04fe35bb0aac9cf6871623ccb4fa55743b86cfaa
afei3 <afei3@illinois.edu>
2018-09-12 20:19:20 -0500

    Implemented algorithm

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -34,6 +34,20 @@ public class SolveMaze {
          * Feel free to adjust this number if you experiment with other mazes.
          */
         for (int step = 0; step < 1000; step++) {
+            maze.turnLeft();
+            if (maze.canMove()) {
+                maze.move();
+            } else {
+                maze.turnRight();
+                while (!maze.canMove()) {
+                    maze.turnRight();
+                }
+                maze.move();
+            }
+            if (maze.isFinished()) {
+                System.out.println(step);
+                break;
+            }
             // Implement your maze solving algorithm here
         }
 
